fca38b6e91a665ddba38ceb41efd5d1b040aa153
Diamond Dog <clm805@gmail.com>
2017-06-19 23:20:09 -0400

    Added pirate ship that follows your mouse (and faces it). Stops moving when it reaches it.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,6 +19,46 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-import { Game } from "./game"
 
-new Game(700, 450, "game")
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+function preload() {
+
+    game.load.image('ball', 'images/pirate.png');
+
+}
+
+var sprite;
+const pi = Phaser.Math.degToRad(180); // FLIPPING PNG'S IS HARD
+
+function create() {
+
+    sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
+    sprite.scale.setTo(.2, .2); // SCALING PNG'S IS HARD
+    sprite.anchor.setTo(.5, .5);
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+}
+
+function update() {
+
+    // //  only move when you click
+    // if (game.input.mousePointer.isDown)
+    // {
+        //  400 is the speed it will move towards the mouse
+        game.physics.arcade.moveToPointer(sprite, 400);
+        sprite.rotation = game.physics.arcade.angleToPointer(sprite) + pi;
+
+        //  if it's overlapping the mouse, don't move any more
+        if (Phaser.Rectangle.contains(sprite.body, game.input.x, game.input.y))
+        {
+            sprite.body.velocity.setTo(0, 0);
+        }
+    // }
+    // else
+    // {
+    //     sprite.body.velocity.setTo(0, 0);
+    // }
+
+}
