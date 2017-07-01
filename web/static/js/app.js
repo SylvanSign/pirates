3e7efc7d9c1d194da29a8d9158614a1eaeb8eb0c
Diamond Dog <clm805@gmail.com>
2017-07-01 15:55:10 -0400

    Rough implementation of drawing other characters on screen. Gonna need to increase push rate. :^)

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -30,25 +30,23 @@ function preload() {
 }
 
 var player;
-var gameWorld;
+var gameState = [];
+var spriteCache = [];
 
 function create() {
 
   game.stage.backgroundColor = "#0000FF";
 
-  player = game.add.sprite(game.world.centerX, game.world.centerY, 'pirate');
-  player.anchor.setTo(.5, .5);
-  game.physics.enable(player, Phaser.Physics.ARCADE);
-  player.body.setCircle(player.width / 2, 0, 0);
+  player = addSprite();
   player.body.debug = true;
 
   game.world.setBounds(0, 0, 2500, 2500);
   game.camera.follow(player);
   game.camera.bounds = null;
-  // gameWorld.scale.set(.5);
 
   gameChannel.on("state_tick", ({ state }) => {
     console.log(JSON.stringify(state));
+    gameState = state;
   })
 }
 
@@ -57,34 +55,41 @@ var trailCounter = trailInterval;
 
 function update() {
 
-  // Move/turn toward pointer
+  /// Input
   game.physics.arcade.moveToPointer(player, 400);
   player.rotation = game.physics.arcade.angleToPointer(player);
-
-  // Stop on mouseover
   if (Phaser.Rectangle.contains(player.body, game.input.worldX, game.input.worldY)) {
     player.body.velocity.setTo(0, 0);
+  }
+
+  for (let char of gameState) {
+    let sprite = spriteCache[char.id] = spriteCache[char.id] || addSprite();
+    sprite.x = char.pos.x;
+    sprite.y = char.pos.y;
+    sprite.rotation = char.rot;
   }
 
   if (trailCounter++ > trailInterval) {
     trailCounter = 0;
 
-    // Draw trail
-    var trail = game.add.graphics(0, 0);
-    for (let x of [0 - game.world.bounds.width, 0, game.world.bounds.width])
-      for (let y of [0 - game.world.bounds.height, 0, game.world.bounds.height]) {
-        trail.beginFill(0xffffff);
-        trail.drawCircle(x + player.x, y + player.y, 3);
-        trail.endFill();
-      }
-    setTimeout(() => trail.kill(), 3000); // Keep trail 3 seconds long
-  }
+    for (let pos of gameState.map(c => c.pos).concat(player)) {
+      // Draw trail
+      var trail = game.add.graphics(0, 0);
+      for (let x of [0 - game.world.bounds.width, 0, game.world.bounds.width])
+        for (let y of [0 - game.world.bounds.height, 0, game.world.bounds.height]) {
+          trail.beginFill(0xffffff);
+          trail.drawCircle(x + pos.x, y + pos.y, 3);
+          trail.endFill();
+        }
+      setTimeout(() => trail.kill(), 3000); // Keep trail 3 seconds long
+    }
 
-  // Wrap player into game world
-  if (player.x > game.world.bounds.width) player.x = 0;
-  if (player.x < 0) player.x = game.world.bounds.width;
-  if (player.y > game.world.bounds.height) player.y = 0;
-  if (player.y < 0) player.y = game.world.bounds.height;
+    // Wrap player into game world
+    if (player.x > game.world.bounds.width) player.x = 0;
+    if (player.x < 0) player.x = game.world.bounds.width;
+    if (player.y > game.world.bounds.height) player.y = 0;
+    if (player.y < 0) player.y = game.world.bounds.height;
+  }
 
   game.debug.body(player);
 
@@ -101,5 +106,13 @@ function render() {
 
 function pushStateToServer() {
   const { body: { position, rotation } } = player;
-  gameChannel.push("player_state", { pos: position, rot: rotation })
+  gameChannel.push("player_state", { pos: position, rot: Phaser.Math.degToRad(rotation) })
+}
+
+function addSprite() {
+  let sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'pirate');
+  sprite.anchor.setTo(.5, .5);
+  game.physics.enable(sprite, Phaser.Physics.ARCADE);
+  sprite.body.setCircle(sprite.width / 2, 0, 0);
+  return sprite;
 }
