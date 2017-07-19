70de056a5795a075852519cc922809e7e944d247
SylvanSign <jake.d.bray@gmail.com>
2017-07-18 23:58:24 -0400

    Travis enhancements (#42)
    * Changes from sandbox fork
    
    * Update .travis.yml
    
    * deploy based on branch
    
    * woops, try all_branches: true
    
    * Updated the secure api-key
    
    * Update prod.exs
    
    * Update .travis.yml

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pirates, Pirates.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: {:system, "HOST"}, port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  check_origin: [
+    "https://pirates-deploy.herokuapp.com/",
+    "https://clm-pirates.herokuapp.com/",
+    "https://jdl-pirates-jb.herokuapp.com/",
+    "https://jake-pirates.herokuapp.com/",
+    "https://zak-pirates.herokuapp.com/",
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +66,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
