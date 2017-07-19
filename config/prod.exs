264eca895cc6b3aae480277d39f79af0fa62a70a
SylvanSign <jake.d.bray@gmail.com>
2017-07-18 23:34:19 -0400

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pirates, Pirates.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "pirates-deploy.herokuapp.com", port: 443],
+  url: [scheme: "https", host: {:system, "HOST"}, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  check_origin: [
+    "https://pirates-deploy.herokuapp.com/",
+    "https://clm-pirates.herokuapp.com/",
+    "https://jdl-pirates-jb.herokuapp.com/",
+    "https://jake-pirates.herokuapp.com/",
+    "https://zak-pirates.herokuapp.com/",
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
